7118d9dc9a7fa98bf9bc21a316c91be0ccca8f26
Chris5alt <chrinacl11@gmail.com>
2024-06-04 16:52:57 -0400

    Finished Project

--- citation-app/scraper.js
+++ citation-app/scraper.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const metascraper = require('metascraper')([
     require('metascraper-author')(),
     require('metascraper-title')(),
@@ -10,70 +11,78 @@ const metascraper = require('metascraper')([
     input: process.stdin,
     output: process.stdout
   });
-  
-  const scrapeMetadata = async (targetUrl) => {
+ 
+  const scrapeMetadata = async () => {
+   
     try {
+      let targetUrl = '';
+      const askUrl = () => {
+        return new Promise((resolve, reject) => {
+          rl.question('\n------------------\nEnter Url: ', (answer) => {
+            targetUrl = answer;
+            resolve();
+          });
+        });
+      };
+      await askUrl();
       // Fetch the HTML of the page
       const { data: html, status } = await axios.get(targetUrl);
       if (status !== 200) {
         console.error('Failed to fetch page, status code:', status);
         return;
       }
-  
+      const date = new Date();
+
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      
+      // This arrangement can be altered based on how we want the date's format to appear.
+      let currentDate = `${month}-${day}-${year}`;
       // Scrape the metadata
       const metadata = await metascraper({ html, url: targetUrl });
   
       let Author = metadata.author;
       let Title = metadata.title;
-      let Date = metadata.date;
+      let Published = metadata.date;
   
       console.log('OUTPUT WEB-SCRAPING DATA:');
       console.log('Author:', Author);
       console.log('Title:', Title);
-      console.log('Date:', Date);
+      console.log('Date:', Published);
       console.log('\n ------------------  \n');
   
+
       const askAuthor = () => {
         return new Promise((resolve, reject) => {
-          rl.question('Would you like to re-enter the author\'s name? (yes/no): ', (answer) => {
-            if (answer.toLowerCase() === 'yes') {
-              rl.question('Enter author\'s name: ', (author) => {
-                Author = author;
-                resolve();
-              });
-            } else {
-              resolve();
-            }
+          rl.question('Reenter author\'s name if you would like to update it: ', (answer) => {
+            if (answer.toLowerCase() !== '') {
+              Author = answer;
+            } 
+            resolve();
           });
         });
       };
+      
   
       const askTitle = () => {
         return new Promise((resolve, reject) => {
-          rl.question('Would you like to re-enter the Title? (yes/no): ', (answer) => {
-            if (answer.toLowerCase() === 'yes') {
-              rl.question('Enter title: ', (title) => {
-                Title = title;
-                resolve();
-              });
-            } else {
-              resolve();
-            }
+          rl.question('Reenter title name if you would like to update it: ', (answer) => {
+            if (answer.toLowerCase() !== '') {
+              Title = answer;
+            } 
+            resolve();
           });
         });
       };
   
       const askDate = () => {
         return new Promise((resolve, reject) => {
-          rl.question('Would you like to re-enter the Date? (yes/no): ', (answer) => {
-            if (answer.toLowerCase() === 'yes') {
-              rl.question('Enter date: ', (date) => {
-                Date = date;
-                resolve();
-              });
-            } else {
-              resolve();
-            }
+          rl.question('Reenter date if you would like to update it: ', (answer) => {
+            if (answer.toLowerCase() !== '') {
+              Published = answer;
+            } 
+            resolve();
           });
         });
       };
@@ -81,20 +90,26 @@ const metascraper = require('metascraper')([
       await askAuthor();
       await askTitle();
       await askDate();
-  
-      console.log("\n UPDATED OUTPUT:");
-      console.log('Author:', Author);
-      console.log('Title:', Title);
-      console.log('Article Date:', Date);
+      
+      var FinalCitation = `\n${Author} on ${Published}` + `["${Title}"][${targetUrl}][Accessed: ${currentDate}][Luke DiPersio]`;
+      console.log(FinalCitation);
 
-  
-      rl.close();
-  
+      fs.appendFile('Citations.txt', FinalCitation, (err) => {
+        if (err) {
+          console.error('An error occurred while writing to Citations.txt:', err);
+        }
+      });
+      
+      scrapeMetadata();
+
     } catch (error) {
       console.error('An error occurred:', error.message);
+      scrapeMetadata();
     }
   };
   
-  const targetUrl = 'https://www.nasa.gov/technology/nasas-network-of-small-moon-bound-rovers-is-ready-to-roll/';
-  scrapeMetadata(targetUrl);
   
+
+  scrapeMetadata();
+  // Declare targetURL variable (new stuff)
+
